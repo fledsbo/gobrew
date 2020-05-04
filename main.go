3beb1ed5cbe1e2d7305709929f0b94958431417b
Øystein Fledsberg <oysteinf@microsoft.com>
2020-05-04 08:17:33 +0200

    Outlet code calculation

--- main.go
+++ main.go
@@ -2,16 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"io/ioutil"
-	"github.com/martinohmann/rfoutlet/pkg/gpio"
-
-	"github.com/paypal/gatt"
-	"github.com/paypal/gatt/examples/option"
-	"github.com/warthog618/gpiod"
-	"github.com/warthog618/gpiod/device/rpi"
+	//"github.com/paypal/gatt"
 )
 
+/*
 func onStateChanged(device gatt.Device, s gatt.State) {
 	switch s {
 	case gatt.StatePoweredOn:
@@ -23,54 +17,56 @@ func onStateChanged(device gatt.Device, s gatt.State) {
 	}
 }
 
-
 func onPeripheralDiscovered(p gatt.Peripheral, a *gatt.Advertisement, rssi int) {
-	tiltIds := map[string]string {
-		"A495BB10-C5B1-4B44-B512-1370F02D74DE":"Red",
-		"A495BB20-C5B1-4B44-B512-1370F02D74DE":"Green",
-		"A495BB30-C5B1-4B44-B512-1370F02D74DE":"Black",
-		"A495BB40-C5B1-4B44-B512-1370F02D74DE":"Purple",
-		"A495BB50-C5B1-4B44-B512-1370F02D74DE":"Orange",
-		"A495BB60-C5B1-4B44-B512-1370F02D74DE":"Blue",
-		"A495BB70-C5B1-4B44-B512-1370F02D74DE":"Yellow",
-		"A495BB80-C5B1-4B44-B512-1370F02D74DE":"Pink",
+	tiltIds := map[string]string{
+		"A495BB10-C5B1-4B44-B512-1370F02D74DE": "Red",
+		"A495BB20-C5B1-4B44-B512-1370F02D74DE": "Green",
+		"A495BB30-C5B1-4B44-B512-1370F02D74DE": "Black",
+		"A495BB40-C5B1-4B44-B512-1370F02D74DE": "Purple",
+		"A495BB50-C5B1-4B44-B512-1370F02D74DE": "Orange",
+		"A495BB60-C5B1-4B44-B512-1370F02D74DE": "Blue",
+		"A495BB70-C5B1-4B44-B512-1370F02D74DE": "Yellow",
+		"A495BB80-C5B1-4B44-B512-1370F02D74DE": "Pink",
 	}
 	b, err := NewiBeacon(a.ManufacturerData)
 	if err != nil {
 		return
 	}
 
-	tilt,ok := tiltIds[b.uuid]
+	tilt, ok := tiltIds[b.uuid]
 	if !ok {
 		return
 	}
 
-	fmt.Printf("%s: %.1f %d\r", tilt, (float64(b.major) - 32) * 5 / 9, b.minor)
+	fmt.Printf("%s: %.1f %d\r", tilt, (float64(b.major)-32)*5/9, b.minor)
 }
+*/
 
 func main() {
 	//log.SetOutput(ioutil.Discard)
 	//device, err := gatt.NewDevice(option.DefaultClientOptions...)
 	//if err != nil {
-		//log.Fatalf("Failed to open device, err: %s\n", err)
-		//return
+	//log.Fatalf("Failed to open device, err: %s\n", err)
+	//return
 	//}
 	//device.Handle(gatt.PeripheralDiscovered(onPeripheralDiscovered))
 	//device.Init(onStateChanged)
 	//select {}
 
-	c, err := gpiod.NewChip("gpiochip0")
-	if err != nil {
-		panic(err)
-	}
+	//c, err := gpiod.NewChip("gpiochip0")
+	//if err != nil {
+	//	panic(err)
+	//}
 
-	offset := rpi.GPIO4
-	transmitter := gpio.NewTransmitter(c, offset, gpio.TransmissionCount(1))
-	defer transmitter.Close()
+	//offset := rpi.GPIO4
+	//transmitter := gpio.NewTransmitter(c, offset, gpio.TransmissionCount(1))
+	//defer transmitter.Close()
+	//
+	//	<-transmitter.Transmit(
 
-	<-transmitter.Transmit(
+	ol1 := NewDialOutlet(0, 0)
+	ol2 := NewDialOutlet(0, 1)
 
-
-
-
+	fmt.Println(ol1.CodeOn, ol1.CodeOff)
+	fmt.Println(ol2.CodeOn, ol2.CodeOff)
 }
---
+++ outlet.go
@@ -0,0 +1,42 @@
+package main
+
+type Outlet struct {
+	CodeOn      uint64
+	CodeOff     uint64
+	Protocol    int
+	PulseLength int
+}
+
+func encode(str []byte) (out uint64) {
+	for _, b := range str {
+		out <<= 2
+		switch b {
+		case 'F':
+			out |= 1
+		case '0':
+			out |= 0
+		case '1':
+			out |= 3
+		}
+	}
+	return
+}
+
+func dialCode(group int, id int, on bool) (out []byte) {
+	out = []byte{'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F', 'F'}
+	out[group] = '0'
+	out[id+4] = '0'
+	if !on {
+		out[11] = '0'
+	}
+	return
+}
+
+func NewDialOutlet(group int, id int) (o *Outlet) {
+	o = new(Outlet)
+	o.Protocol = 1
+	o.PulseLength = 350
+	o.CodeOn = encode(dialCode(group, id, true))
+	o.CodeOff = encode(dialCode(group, id, false))
+	return
+}
